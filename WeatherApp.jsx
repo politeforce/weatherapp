f39931cb57b4424a664b4ba7863f68018cd14d6e
Ryan Taheri <politeforce@csu.fullerton.edu>
2026-05-02 21:16:32 -0700

    emoji fix

--- WeatherApp.jsx
+++ WeatherApp.jsx
@@ -85,8 +85,8 @@ const todayHours = weather
     if (code <= 2) return isNight ? "☁️" : "🌤️"; 
     if (code === 3) return "☁️"; 
     if (code >= 45 && code <= 48) return "🌫️"; 
-    if (code >= 51 && code <= 67) return "🌧️"; 
-    if (code >= 71 && code <= 77) return "❄️"; 
+    if ((code >= 80 && code <= 82) || (code >= 51 && code <= 67)) return "🌧️"; 
+    if ((code >= 85 && code <= 86) || (code >= 71)) return "❄️"; 
     if (code >= 95) return "⛈️"; 
     return "❓";
   };
